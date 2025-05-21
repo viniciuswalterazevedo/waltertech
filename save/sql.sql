65b75d53dd6b42310892363b7f576462c71a2b5a
viniciuswalterazevedo <129801942+viniciuswalterazevedo@users.noreply.github.com>
2025-05-21 19:47:37 -0300

    Save 21.05.2025

---
+++ save/sql.sql
@@ -0,0 +1,18 @@
+create database waltertech;
+use waltertech; 
+
+create table usuarios(
+	id_usuario int primary key auto_increment not null,
+    nome varchar(100) not null,
+    email varchar(100) not null,
+    senha varchar(100) not null
+);
+
+create table servico (
+	id_servico int primary key auto_increment not null,
+    id_usuario int not null,
+    cliente varchar(100) not null,
+    data_criacao datetime not null default current_timestamp,
+    valor varchar(100) not null default '0',
+    foreign key (id_usuario) references usuarios(id_usuario) on update cascade on delete no action
+);
